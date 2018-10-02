bf913e52b539f4217160d24759ffbb9094c288bd
Maximilian Nowak <maximilian.nowak@contiweg.at>
2018-10-02 21:18:31 +0200

    Add al data

---
+++ Assets/Scripts/Camaeramotor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camaeramotor : MonoBehaviour
+{
+
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // tis is to check if the camrea have to move on the X
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+        // tis is to check if the camrea have to move on the Y 
+        float delatY = lookAt.position.y - transform.position.y;
+        if (delatY > boundY || delatY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = delatY - boundY;
+            }
+            else
+            {
+                delta.y = delatY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D BoxColider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    private void Start()
+    {
+        BoxColider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // reset move delta
+        moveDelta = new Vector3(x,y,0);
+
+        // Swap directions between left and right
+        if (moveDelta.x > 0) 
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //chek if the way is free to go with boxcast if box returns null move is able 
+        hit = Physics2D.BoxCast(transform.position, BoxColider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("actor", "blocking"));      
+        if(hit.collider == null)
+        { 
+
+            //movment for the player
+            transform.Translate(0 ,moveDelta.y * Time.deltaTime ,0);
+
+        }
+
+        hit = Physics2D.BoxCast(transform.position, BoxColider.size, 0, new Vector2(moveDelta.x ,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("actor", "blocking"));
+        if (hit.collider == null)
+        {
+
+            //movment for the player
+            transform.Translate(moveDelta.x * Time.deltaTime, 0,0);
+
+        }
+    }
+
+}
